36c31badd3d32e7a2608510c28e67bb68f59c061
Pedro Ramos <medpsr9@gmail.com>
2021-11-08 04:01:48 -0600

    complete assignment

--- spec/html_spec.rb
+++ spec/html_spec.rb
@@ -1,8 +1,16 @@
-require_relative './spec_helper'
-
+<!DOCTYPE HTML>
+<HTML>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<title>Cat Perry's Favorite Cats</title>
+</head>
+<body>
 describe "A populated HTML file called my_list.html" do
   it 'should be present' do
     file_is_present = File.exists?("my_list.html")
     expect(file_is_present).to be_truthy, "Make sure you created a file called my_list.html with your HTML document in it!"
   end
 end
+</body>
+<!-- This is a comment! -->
+<HTML>
